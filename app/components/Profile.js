ccadad4ff2c910fe163f80068ff20d0118a6affd
elias <https://bitbucket.org/EliosMoncho/repo360/src/master/>
2022-12-19 16:18:40 -0700

    showing not found comp when user mess with the url

--- app/components/Profile.js
+++ app/components/Profile.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useContext } from "react"
 import Page from "./Page"
+import NotFound from "./NotFound"
 import { useParams, NavLink, Routes, Route } from "react-router-dom"
 import Axios from "axios"
 import StateContext from "../StateContext"
@@ -108,41 +109,43 @@ function Profile() {
     })
   }
 
-  return (
-    <Page title="Profile Screen">
-      <h2>
-        <img className="avatar-small" src={state.profileData.profileAvatar} /> {state.profileData.profileUsername}
-        {appState.loggedIn && !state.profileData.isFollowing && appState.user.username != state.profileData.profileUsername && state.profileData.profileUsername != "..." && (
-          <button onClick={startFollowing} disabled={state.followActionLoading} className="btn btn-primary btn-sm ml-2">
-            Follow <i className="fas fa-user-plus"></i>
-          </button>
-        )}
-        {appState.loggedIn && state.profileData.isFollowing && appState.user.username != state.profileData.profileUsername && state.profileData.profileUsername != "..." && (
-          <button onClick={stopFollowing} disabled={state.followActionLoading} className="btn btn-danger btn-sm ml-2">
-            Stop Following <i className="fas fa-user-times"></i>
-          </button>
-        )}
-      </h2>
+  if (!response.data) return <NotFound />
+  else
+    return (
+      <Page title="Profile Screen">
+        <h2>
+          <img className="avatar-small" src={state.profileData.profileAvatar} /> {state.profileData.profileUsername}
+          {appState.loggedIn && !state.profileData.isFollowing && appState.user.username != state.profileData.profileUsername && state.profileData.profileUsername != "..." && (
+            <button onClick={startFollowing} disabled={state.followActionLoading} className="btn btn-primary btn-sm ml-2">
+              Follow <i className="fas fa-user-plus"></i>
+            </button>
+          )}
+          {appState.loggedIn && state.profileData.isFollowing && appState.user.username != state.profileData.profileUsername && state.profileData.profileUsername != "..." && (
+            <button onClick={stopFollowing} disabled={state.followActionLoading} className="btn btn-danger btn-sm ml-2">
+              Stop Following <i className="fas fa-user-times"></i>
+            </button>
+          )}
+        </h2>
 
-      <div className="profile-nav nav nav-tabs pt-2 mb-4">
-        <NavLink to="" end className="nav-item nav-link">
-          Posts: {state.profileData.counts.postCount}
-        </NavLink>
-        <NavLink to="followers" className="nav-item nav-link">
-          Followers: {state.profileData.counts.followerCount}
-        </NavLink>
-        <NavLink to="following" className="nav-item nav-link">
-          Following: {state.profileData.counts.followingCount}
-        </NavLink>
-      </div>
+        <div className="profile-nav nav nav-tabs pt-2 mb-4">
+          <NavLink to="" end className="nav-item nav-link">
+            Posts: {state.profileData.counts.postCount}
+          </NavLink>
+          <NavLink to="followers" className="nav-item nav-link">
+            Followers: {state.profileData.counts.followerCount}
+          </NavLink>
+          <NavLink to="following" className="nav-item nav-link">
+            Following: {state.profileData.counts.followingCount}
+          </NavLink>
+        </div>
 
-      <Routes>
-        <Route path="" element={<ProfilePosts />} />
-        <Route path="followers" element={<ProfileFollowers />} />
-        <Route path="following" element={<ProfileFollowing />} />
-      </Routes>
-    </Page>
-  )
+        <Routes>
+          <Route path="" element={<ProfilePosts />} />
+          <Route path="followers" element={<ProfileFollowers />} />
+          <Route path="following" element={<ProfileFollowing />} />
+        </Routes>
+      </Page>
+    )
 }
 
 export default Profile

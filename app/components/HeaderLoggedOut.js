8c1ea5c3a8cdbbf589b2752b94f9ca106fc869ae
elias <https://bitbucket.org/EliosMoncho/repo360/src/master/>
2022-12-19 21:24:02 -0700

    removed length from uN and pass

--- app/components/HeaderLoggedOut.js
+++ app/components/HeaderLoggedOut.js
@@ -9,11 +9,11 @@ function HeaderLoggedOut(props) {
 
   const handleSubmit = async e => {
     e.preventDefault()
-    if (username.length < 1) {
+    if (!username) {
       appDispatch({ type: "flashMessage", value: "Username required" })
       return
     }
-    if (password.length < 1) {
+    if (!password) {
       appDispatch({ type: "flashMessage", value: "Password required" })
       return
     }
@@ -31,10 +31,10 @@ function HeaderLoggedOut(props) {
     <form onSubmit={handleSubmit} className="mb-0 pt-2 pt-md-0">
       <div className="row align-items-center">
         <div className="col-md mr-0 pr-md-0 mb-3 mb-md-0">
-          <input onChange={e => setUsername(e.target.value)} name="username" className={"form-control form-control-sm input-dark " + password.length < 1 ? "is-invalid" : ""} type="text" placeholder="Username" autoComplete="off" />
+          <input onChange={e => setUsername(e.target.value)} name="username" className={"form-control form-control-sm input-dark " + !username ? "is-invalid" : ""} type="text" placeholder="Username" autoComplete="off" />
         </div>
         <div className="col-md mr-0 pr-md-0 mb-3 mb-md-0">
-          <input onChange={e => setPassword(e.target.value)} name="password" className={"form-control form-control-sm input-dark " + password.length < 1 ? "is-invalid" : ""} type="password" placeholder="Password" />
+          <input onChange={e => setPassword(e.target.value)} name="password" className={"form-control form-control-sm input-dark " + !password ? "is-invalid" : ""} type="password" placeholder="Password" />
         </div>
         <div className="col-md-auto">
           <button className="btn btn-success btn-sm">Sign In</button>
